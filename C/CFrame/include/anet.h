ccc71e8e42b61fde7acef7400fa6bb9b473b9459
yangqiang <yangqiangjob@foxmail.com>
2017-02-11 14:52:54 +0800

    anet

---
+++ C/CFrame/include/anet.h
@@ -0,0 +1,22 @@
+/*****************************************************************************
+Copyright: 2017~2030 yangqiang personal
+File name: vector.h
+Description: Basic Tcp socket stuff made a bit less boring .Derived from redis
+Author: yangqiang
+Version: 1.0
+Date:
+History:
+
+vector结构和操作逻辑声明:
+1. 在一整片(size*slots)连续的内存中，模拟出数组的行为。
+2. vecotr与普通数组的区别是，vector容量不足时，可以动态的扩充容量。
+
+
+*****************************************************************************/
+
+
+#ifndef __ANET_H_
+#define __ANET_H_
+
+
+#endif
