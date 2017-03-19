3f82762f518b43c95e07c53ae60e117c44bd58c0
yylover <yangqiangjob@foxmail.com>
2017-03-20 00:51:57 +0800

    hash

--- C/CFrame/include/hash.h
+++ C/CFrame/include/hash.h
@@ -1,6 +1,49 @@
 #ifndef __HASH_H__
 #define __HASH_H__
 
+//重新分配空间的大小比例
+const int HASH_RESIZE_RATIO = 1; //重新分配空间的比例
+const int HASH_INIT_SIZE = 32; //最小分配空间
+
+//最什么没有给默认的set free 函数，因为不确定是否一定是可以用free释放的
+#define HASH_SET_SETKEY(ht, func) (ht)->setKey = (func)
+#define HASH_SET_SETVAL(ht, func) (ht)->setVal = (func)
+#define HASH_SET_KEYCMP(ht, func) (ht)->keyCmp = (func)
+#define HASH_SET_FREEKEY(ht, func) (ht)->freeKey = (func)
+#define HASH_SET_FREEVAL(ht, func) (ht)->freeVal = (func)
+
+#define HASH_SET_KEY(ht, he, _key_) do {\
+    if ((ht)->setKey) {\
+        (he)->key = (ht)->setKey(_key_);\
+    } else {\
+        (he)->key = (_key_);\
+    }\
+} while(0)
+
+#define HASH_SET_VAL(ht, he, _val_) do {\
+    if ((ht)->setVal) {\
+        (he)->value = (ht)->setVal(_val_);\
+    } else {\
+        (he)->value = (_val_);\
+    }\
+} while(0)
+
+#define HASH_FREE_KEY(ht, he) do {\
+    if ((ht)->freeKey) {\
+        (ht)->freeKey((he)->key);\
+    } \
+} while (0)
+
+#define HASH_FREE_VAL(ht, he) do {\
+    if ((ht)->freeVal) {\
+        (ht)->freeVal((he)->value);\
+    } \
+} while (0)
+
+#define HASH_CMP_KEYS(ht, key1, key2) \
+    (ht)->keyCmp ? (ht)->keyCmp((key1), (key2)) : (key1) == (key2)
+
+
 // hash 的节点
 typedef struct hashEntry {
     void *key;
@@ -11,8 +54,8 @@ typedef struct hashEntry {
 //hash 结构体
 typedef struct hash {
     HashEntry **datas;
-    int capcity; //
-    int size;    //长度
+    unsigned int capcity; //
+    unsigned int size;    //长度
 
     int (*keyCmp) (const void *, const void *);
     void *(*setKey) (const void *);
@@ -32,12 +75,14 @@ typedef struct hashIter {
 } HashIter;
 
 HashTable * hashCreate(int capcity);
-int hashInsert(HashTable *ht, const void *key, const void *value);
+int hashSet(HashTable *ht, const void *key, const void *value);
 int hashRemove(HashTable *ht, const void *key);
 int hashForEach(HashTable *ht, int (*foreach)(const HashEntry *entry, void *userptr), void *userptr);
-int hashGet(HashTable *ht, const void *key);
+void* hashGet(HashTable *ht, const void *key);
+void hashClear(HashTable *ht);   //清空所有数据
 void hashDestroy(HashTable *ht); //删除table
-void hashClear(HashTable *ht); //清空所有数据
+void hashFree(HashTable *ht);    //
+HashTable *hashDuplicate(HashTable *ht);
 
 HashIter *hashIterCreate(HashTable *ht); //创建遍历
 int hashIterMoveNext(HashIter *iter);   //向后移动
@@ -45,7 +90,7 @@ int hashIterMovePrev(HashIter *iter);   //向前移动
 int hashIterReset(HashIter *iter);      //重置位置
 void hashIterFree(HashIter *iter);      //释放
 
-//默认的key set 和free函数
+//默认的key set 和free函数,默认都是作为字符串，free方式释放
 void *hashDefaultSet(const void *key);
 void hashDefaultFree(void *key);
 int hashDefaultCmp(const void *key1, const void *key2);
--- C/CFrame/src/hash.c
+++ C/CFrame/src/hash.c
@@ -1,6 +1,15 @@
 #include "../include/hash.h"
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <limits.h>
 
+/**
+ * 1. 哈希表插入，要判断空间，如果空间达到一定的比例，要重新分配更大的空间
+ * 2. 哈希表插入，如果原来有key, 则直接替换原来的值
+ *
+ */
 
 /**
  * 功能: hash散列函数
@@ -11,10 +20,160 @@ static unsigned int hashFunc(const void *key)
 {
     unsigned int h = 5381;
     char *ptr = (char *)key;
-    while(*ptr != '\0') {
+    while (*ptr != '\0') {
         h = (h+(h<<5)) + (*ptr++);
     }
     return h;
+}
+
+/**
+ * 获取大于n的最小的2^幂
+ *
+ * 初始化存储空间的时候是安装2的幂次方非配空间的
+ * @param  n capacity
+ * @return
+ */
+static unsigned int getNextPower(unsigned long size) {
+    //溢出判断
+    if (size >= LONG_MAX / 2) {
+        return size;
+    }
+
+    unsigned long res = HASH_INIT_SIZE;
+    while (res < size) {
+        res <<= 1;
+    }
+    return res;
+}
+
+/**
+ * 哈希表初始化
+ * @param  ht      哈希表
+ * @param  capcity 容量
+ * @return
+ */
+static int hashInit(HashTable *ht, unsigned int capcity) {
+    assert(ht);
+
+    ht->capcity  = getNextPower(capcity);
+
+    ht->datas  = (HashEntry **)calloc(ht->capcity, sizeof(HashEntry*));
+    if (!ht->datas) {
+        return -1;
+    }
+    return 0;
+}
+
+/**
+ * 哈希表插入一个entry节点
+ *
+ * 采用头插法
+ * @param  ht    哈希表
+ * @param  entry entry节点
+ * @return       [description]
+ */
+static int hashInsert(HashTable *ht, HashEntry *entry) {
+    assert(ht && entry);
+
+    int index = hashFunc(entry->key) % ht->capcity;
+    HashEntry *he = ht->datas[index];
+    entry->next = he;
+    ht->datas[index] = entry;
+    ht->size++;
+
+    return 0;
+}
+
+/**
+ * 哈希表创建一个节点
+ * @param  ht  HashTable
+ * @param  key
+ * @param  val
+ * @return
+ */
+static HashEntry * hashCreateEntry(HashTable *ht, const void *key, const void *val) {
+
+    HashEntry *he = (HashEntry *) calloc(1, sizeof(*he));
+    if (he == NULL) {
+        return NULL;
+    }
+
+    he->next = NULL;
+    HASH_SET_KEY(ht, he, (void *)key);
+    HASH_SET_VAL(ht, he, (void *)val);
+    // he->key = ht->setKey(key);
+    // he->value = ht->setVal(val);
+
+    return he;
+}
+
+/**
+ * 释放哈希表的一个节点
+ * @param  ht
+ * @param  he
+ * @return
+ */
+static int hashFreeEntry(HashTable *ht, HashEntry *he) {
+    HASH_FREE_KEY(ht, he);
+    HASH_FREE_VAL(ht, he);
+
+    he->next = NULL;
+    free(he); //he 指向的空间也要释放
+    he = NULL;
+    ht->size--;
+
+    return 0;
+}
+
+/**
+ * 将哈希表的空间扩大两倍,并释放原来的空间
+ *
+ * 复用了原来的哈希表节点，只对datas重新分配空间
+ * 哈希表扩容时，原来的需要重新插入
+ * @param       ht 哈希表
+ * @return     HashTable *
+ */
+static int hashResize(HashTable *ht) {
+    assert(ht);
+
+    int capacity = ht->capcity;
+    HashEntry **datas = ht->datas;
+
+    if (hashInit(ht, ht->capcity*2) != 0) {
+        //内存分配失败
+        ht->datas = datas;
+        return -1;
+    }
+
+    //遍历ht元素插入到新的哈希表中
+    int i = 0;
+    HashEntry *t = NULL, *temp = NULL;
+    for (i = 0; i < capacity; i++) {
+        t = datas[i];
+        while (t != NULL) {
+            temp = t;
+            t = t->next;
+            hashInsert(ht, temp);
+        }
+    }
+
+    //释放原来的空间
+    if (datas) {
+        free(datas);
+    }
+
+    return 0;
+}
+
+/**
+ * 对每个HashEntry 执行删除
+ * @param  entry
+ * @param  userptr
+ * @return
+ */
+int foreachDeleteEntry(const HashEntry *entry, void *userptr) {
+    HashTable *ht = (HashTable *)userptr;
+    return hashRemove(ht, entry->key);
 }
 
 /**
@@ -40,48 +199,156 @@ void hashDefaultFree(void *key) {
  * @return      [description]
  */
 int hashDefaultCmp(const void *key1, const void *key2) {
-    return strcmp(key1, key2);
+    return strcmp(key1, key2) == 0;
 }
 
+/**
+ * 创建哈希表
+ * @param  capcity [description]
+ * @return         [description]
+ */
 HashTable * hashCreate(int capcity) {
     HashTable *ht = (HashTable *)malloc(sizeof(*ht));
-    if (ht == NULL) {
+    if (!ht) {
         return NULL;
     }
+
     ht->capcity = capcity;
     ht->size = 0;
-    ht->datas = (HashEntry **)calloc(capcity, sizeof(HashEntry*));
+    if (hashInit(ht, capcity) != 0) {
+        free(ht);
+        ht = NULL; //指针指空
+        return NULL;
+    }
+
+    ht->keyCmp = hashDefaultCmp;
+    ht->setKey = hashDefaultSet;
+    ht->setVal = hashDefaultSet;
+    ht->freeKey = hashDefaultFree;
+    ht->freeVal = hashDefaultFree;
 
     return ht;
 }
 
+/**
+ * 插入元素
+ * @param  ht    [description]
+ * @param  key   [description]
+ * @param  value [description]
+ * @return       [description]
+ */
+int hashSet(HashTable *ht, const void *key, const void *value) {
 
-int hashInsert(HashTable *ht, const void *key, const void *value) {
+    if ((ht->size / ht->capcity) >= 1) {
+        if (hashResize(ht) != 0) {
+            return -1;
+        };
+    }
+
+    //查找原来是否有此元素
+    int index = hashFunc(key) % ht->capcity;
+    HashEntry *he = ht->datas[index];
+    while (he) {
+        if (HASH_CMP_KEYS(ht, he->key, key)) {
+            HASH_FREE_VAL(ht, he);//** 要对原来的值进行释放
+            HASH_SET_VAL(ht, he, (void *)value);
+            return 0;
+        }
+        he = he->next;
+    }
+
+    if ((he = hashCreateEntry(ht, key, value)) == NULL) {
+        return -1;
+    }
+
+    if (hashInsert(ht, he) != 0) {
+        hashFreeEntry(ht, he);
+        return -1;
+    }
 
     return 0;
-}
-
-
-int hashRemove(HashTable *ht, const void *key) {
-
-    return 0;
-}
-
-int hashForEach(HashTable *ht, int (*foreach)(const HashEntry *entry, void *userptr), void *userptr) {
-
-    return 0;
-}
-
-int hashGet(HashTable *ht, const void *key) {
-
 }
 
 /**
- * 销毁ht 所分配的空间
- * @param ht [description]
+ * 删除哈希表元素
+ * @param  ht  哈希表
+ * @param  key key
+ * @return
  */
-void hashDestroy(HashTable *ht) {
+int hashRemove(HashTable *ht, const void *key) {
+    assert(ht && key);
 
+    HashEntry *pre = NULL, *ptr = NULL;
+    int index = hashFunc(key) % ht->capcity;
+
+    ptr = ht->datas[index];
+    while (ptr) {
+        if (HASH_CMP_KEYS(ht, ptr->key, key)) {
+            break;
+        }
+        pre = ptr;
+        ptr = ptr->next;
+    }
+
+    if (ptr) {
+        if (pre) {
+            pre->next = ptr->next;
+        } else {
+            ht->datas[index] = ptr->next;
+        }
+        hashFreeEntry(ht, ptr);
+        return 0;
+    }
+
+    return -1;
+}
+
+/**
+ * 查找哈希表表元素
+ *
+ * 查找和删除都要有查找的过程
+ * @param ht
+ * @param key
+ */
+void* hashGet(HashTable *ht, const void *key) {
+    int index = hashFunc(key) % ht->capcity;
+
+    HashEntry *ptr = ht->datas[index];
+    while (ptr) {
+        if (HASH_CMP_KEYS(ht, ptr->key, key)) {
+            return ptr->value;
+        }
+        ptr = ptr->next;
+    }
+
+    return NULL;
+}
+
+
+/**
+ * 对哈希表的每个元素执行函数
+ *
+ * 函数设计并不好，不用实现这样的函数，HashEntry可以认为是内部的数据结构
+ * @param  ht      哈希表吧
+ * @param  foreach
+ * @return         [description]
+ */
+int hashForEach(HashTable *ht, int (*foreach)(const HashEntry *entry, void *userptr), void *userptr) {
+    assert(ht && foreach);
+
+    HashEntry *he = NULL;
+    for (int i = 0; i < ht->capcity; i++) {
+        he = ht->datas[i];
+        while (he) {
+            if (foreach(he, userptr) != 0) {
+                return -1;
+            }
+            //TODO 可能有bug,因为Foreach可能执行删除函数
+            he = he->next;
+        }
+    }
+
+    return 0;
 }
 
 /**
@@ -89,5 +356,90 @@ void hashDestroy(HashTable *ht) {
  * @param ht [description]
  */
 void hashClear(HashTable *ht) {
+    hashForEach(ht, foreachDeleteEntry, ht);
+    memset(ht->datas, 0, sizeof(HashEntry *)*ht->capcity);
+    ht->size = 0;
+}
 
+/**
+ * 销毁ht 所分配的空间,不释放ht
+ * @param ht [description]
+ */
+void hashDestroy(HashTable *ht) {
+    hashForEach(ht, foreachDeleteEntry, ht);
+    free(ht->datas);
+
+    ht->datas = NULL;
+    ht->size = 0;
+    ht->capcity = 0;
+
+    ht->setKey = NULL;
+    ht->setVal = NULL;
+    ht->freeKey = NULL;
+    ht->freeVal = NULL;
+    ht->keyCmp = NULL;
+}
+
+/**
+ * 释放哈希表
+ * @param ht
+ */
+void hashFree(HashTable *ht) {
+    hashDestroy(ht);
+    free(ht);
+    ht = NULL;
+}
+
+static int hashDupEach(const HashEntry *he, void *userdata) {
+    HashTable *ht = (HashTable *) userdata;
+    return hashSet(ht, he->key, he->value);
+}
+
+
+HashTable *hashDuplicate(HashTable *ht) {
+    HashTable *htNew = hashCreate(ht->capcity);
+    if (!ht) {
+        return NULL;
+    }
+
+    HASH_SET_SETKEY(htNew, ht->setKey);
+    HASH_SET_SETVAL(htNew, ht->setVal);
+    HASH_SET_FREEKEY(htNew, ht->freeKey);
+    HASH_SET_FREEVAL(htNew, ht->freeVal);
+    HASH_SET_KEYCMP(htNew, ht->keyCmp);
+
+    if (hashForEach(ht, hashDupEach, (void*)htNew) != 0) {
+        hashFree(htNew);
+        htNew = NULL;
+    }
+    return htNew;
+}
+
+
+
+void print_str(char *str) {
+    while (*str != '\0') {
+        printf("%c", *str);
+        str++;
+    }
+    printf("\n");
+}
+
+int main() {
+    char *str = "hello world";
+    print_str(str);
+    char *str2 = "502341152355";
+    print_str(str2);
+
+    char *str3 = (char *) malloc(10 *sizeof(char));
+    strcpy(str3, "hello");
+    print_str(str3);
+
+    printf("%d\n", (int)str3);
+    char *str4 =realloc(str3, 20);
+    printf("%s\n", str4);
+    printf("%s\n", str3);
+    printf("%d\n", (int)str3);
+
+    return 0;
 }
