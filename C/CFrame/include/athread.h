116f92130e1fe92954e39e57f7b82788ba969548
yylover <yangqiangjob@foxmail.com>
2017-03-23 01:06:28 +0800

    线程

--- C/CFrame/include/athread.h
+++ C/CFrame/include/athread.h
@@ -6,26 +6,28 @@
 typedef enum workerStat {
     W_RUNNING,
     W_STOPING,
-    W_STOP
+    W_STOPED
 } workerStat;
+
+struct threadPool;
 
 typedef struct threadWorker {
     int idx;// 在worker列表的索引位置
     pthread_t tid; //线程id
-    int notify_read_fd;//master worker 线程通信管道读取端
-    int notify_write_fd; //master worker 线程通信管道写端
-    worker_stat_t status; //线程工作状态
+    int notifyReadFd;//master worker 线程通信管道读取端
+    int notifyWriteFd; //master worker 线程通信管道写端
+    workerStat status; //线程工作状态
 
     aeEventLoop   *ael; //ae句柄
-    threadPool *pool; //线程池指针
-    adlist *client_list; //本线程负责的client
-    void *priv_data; //worker 自身数据
+    threadPool    *pool; //线程池指针
+    adlist        *client_list; //本线程负责的client
+    void          *priv_data; //worker 自身数据
 } threadWorker;
 
 typedef struct threadPool {
     mutexLock *lock;
 
-    threadWorker **thread_workers; //数组
+    threadWorker **threadWorkers; //数组
     int threadWorkerNum; //线程数量
 
     int threadStackSize; //线程栈大小
@@ -35,10 +37,12 @@ typedef struct threadPool {
 
 threadPool *threadPoolCreate(int workerNum, int stackSize, void* (*workerTask)(void *arg));
 int threadPollDestroy(threadPool *pool);
-void *thread_worker_cycle(void *arg);
-void threadSleep(int seconds);
-void threadUnsleep(int useconds);
+void *threadWorkerCycle(void *arg);
 threadWorker *getWorker(int workerId);
+
+void threadSleep(int seconds);
+void threadUsecsleep(int useconds);
+
 void writeToClient(aeEventLoop *el, int fd, void *privdata, int mask);
 
 #endif
--- C/CFrame/src/athread.c
+++ C/CFrame/src/athread.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,45 +11,80 @@
 #include <fcntl.h> //文件属性修改
 #include <netinet/tcp.h>
 
-#include "../athread.h"
+#include "../include/athread.h"
 
 #define IOBUF_SIZE 4096
 #define MAX_PROT_LEN 10485760 /* 10M  最大请求包*/
 
-extern instance_t *g_ins;
+// extern instance_t *g_ins;
 
-static void thread_worker_process_notify(struct aeEventLoop *el, int fd, void *clientdata, int mask);
-static void thread_worker_create(thread_pool_t *pool, void *(*worker_task)(void *arg));
-static void read_from_client(aeEventLoop *el, int fd, void *priv_data, int mask);
-static int worker_cron(aeEventLoop *el, long long id, void *priv_data);
-static void thread_worker_shutdown(thread_worker_t *worker);
+static int threadWorkerCreate(threadPool *pool, void *(*worker_task)(void *arg), int idx);
+static int threadWorkerDestroy(threadWorker *worker);
+static void threadWorkerProcessNotify(struct aeEventLoop *el, int fd, void *clientdata, int mask);
+static void readFromClient(aeEventLoop *el, int fd, void *priv_data, int mask);
+static int workerCron(aeEventLoop *el, long long id, void *priv_data);
+static void threadWorkerShutdown(thread_worker_t *worker);
 
 
-thread_pool_t *threadPoolCreate(int worker_num, int stack_size, void* (*worker_task)(void *arg)); {
+static int threadWorkerCreate(threadPool *pool, void *(*workerTask)(void *arg), int idx) {
+    assert(pool != NULL && workerTask != NULL);
+
+    threadWorker *worker = NULL;
+    if (NULL == (worker = calloc(1, *worker))) {
+        return -1;
+    }
+
+    worker->idx = idx;
+    worker->status = W_STOPED;
+    worker->priv_data = NULL;
+    worker->pool = pool;
+
+    pthread_attr_t attr ;
+    if (pthread_attr_init(&attr) != 0) {
+        free(worker);
+        return -1;
+    }
+
+    if (pthread_attr_setstacksize(&attr, THREAD_STACK_SIZE) != 0) {
+        pthread_attr_destroy(&attr);
+        free(worker);
+        return -1;
+    }
+
+    if (pthread_create(&worker->tid, &attr, workerTask, NULL) != -1) {
+        pthread_attr_destroy(&attr);
+        free(worker);
+        return -1;
+    }
+
+    pthread_attr_destroy(&attr);
+    return 0;
+}
+
+threadPool *threadPoolCreate(int workerNum, int stackSize, void* (*workerTask)(void *arg)) {
     assert(worker_num > 0 && stack_size > 0);
 
-    thread_pool_t *pool = NULL;
-
+    threadPool *pool = NULL;
     if (NULL == (poll = calloc(1, sizeof(*poll)))) {
         return NULL;
     }
 
-    //初始化线程锁
-    if (NULL == (thread_workers = calloc(worker_num, sizeof(thread_worker_t)))) {
+    if (NULL == (pool->threadWorkers = calloc(workerNum, sizeof(threadWorker)))) {
         free(pool);
         return NULL;
     }
 
-    pool->thread_worker_num = worker_num;
-    pool->thread_stack_size = stack_size;
-    pool->client_timeout  = 0; //获取配置
-    pool->poll_interval = 0;   // 配置配置文件
+    pool->threadWorkerNum = workerNum;
+    pool->threadStackSize = stackSize;
+    pool->clientTimeout   = 10; //获取配置
+    pool->pollInterval    = 10; // 配置配置文件
 
     //初始化worker 线程
     int i = 0;
-    for (i = 0; i < worker_num; i++) {
-        if (thread_worker_create(pool, worker_task, i) == NULL) {
-            //打印错误日志
+    for (i = 0; i < workerNum; i++) {
+        if (threadWorkerCreate(pool, workerTask, i) != 0) {
+            //TODO threadWorker 释放
+            free(pool);
             return NULL;
         }
     }
@@ -58,28 +92,34 @@ thread_pool_t *threadPoolCreate(int worker_num, int stack_size, void* (*worker_t
     return pool;
 }
 
+/**
+ * 线程池销毁函数
+ * @param  pool [description]
+ * @return      [description]
+ */
 int threadPollDestroy(thread_pool_t *pool) {
     assert(pool != NULL);
 
     int i = 0;
-    thread_worker_t *worker;
-
-    //加锁
     for (; i < pool->worker_num; i++) {
-        worker = pool->thread_workers[i];
+        threadWorker *worker = pool->thread_workers[i];
         if (worker) {
-            free(worker);
+            threadWorkerDestroy(worker);
         }
     }
     //解锁
 
     //释放锁
-    free(pool->thread_workers);
+    free(pool->threadWorkers);
     free(pool);
 
     return 0;
 }
 
+/**
+ * 线程休眠秒数
+ * @param seconds
+ */
 void threadSleep(int seconds) {
     struct timeval timeout;
     timeout.tv_sec = seconds;
@@ -87,24 +127,36 @@ void threadSleep(int seconds) {
     select(0, NULL, NULL, NULL, &timeout);
 }
 
-void threadUnsleep(int useconds) {
+/**
+ * 线程休眠毫秒数
+ * @param useconds
+ */
+void threadUsecsleep(int useconds) {
     struct timeval timeout;
     timeout.tv_usec = useconds;
     timeout.tv_sec = 0;
     select(0, NULL, NULL, NULL, &timeout);
 }
 
-thread_worker_t *getWorker(int worker_id) {
-    if (worker_id < 0 || worker_id > g_ins->pool->thread_worker_num ) {
+/**
+ * 获取工作线程
+ * @param  worker_id [description]
+ * @return           [description]
+ */
+threadWorker *getWorker(threadPool *pool, int workerId) {
+    if (workerId < 0 || workerId > pool->threadWorkerNum ) {
         return NULL;
     }
 
-    thread_woker_t *worker = NULL;
-    worker = (thread_worker_t *) (g_ins->pool->thread_works + worker_id);
-    return worker;
+    return (threadWorker *) (pool->threadWorkers + workerId);
 }
 
-void *thread_worker_cycle(void *arg) {
+/**
+ * 线程池线程工作函数
+ * @param  arg [description]
+ * @return     [description]
+ */
+void *threadWorkerCycle(void *arg) {
 
 }
 
