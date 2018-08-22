6784420b5a664bcbd8e1c398849b4654c1d747ab
yangqiang <quickyang@tencent.com>
2018-08-22 22:35:11 +0800

    push

--- go/src/signal/signal.go
+++ go/src/signal/signal.go
@@ -1,38 +1,82 @@
 package signal
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
+	"syscall"
+)
+
+var (
+	signals = []os.Signal{
+		syscall.SIGTERM,
+		syscall.SIGINT,
+		syscall.SIGQUIT,
+		syscall.SIGUSR1,
+		syscall.SIGUSR2,
+		syscall.SIGKILL, //不能捕获
+	}
+)
+
+const (
+	SIGTERM = iota
+	SIGINT
+	SIGQUIT
+	SIGUSR1
+	SIGUSR2
+	SIGKILL //不能捕获
 )
 
 type SignalCatcher struct {
-	quitChan    chan bool
-	sigChan chan os.Signal
-	handlerMap map[os.Signal] func()
+	quitChan   chan bool
+	sigChan    chan os.Signal
+	handlerMap map[os.Signal]func()
 }
 
 func NewSignalCatcher() *SignalCatcher {
-	return &SignalCatcher{
-		quitChan: make(chan bool),
+	sc := &SignalCatcher{
+		quitChan:   make(chan bool),
+		handlerMap: make(map[os.Signal]func()),
+		sigChan:    make(chan os.Signal),
 	}
+
+	return sc
+}
+
+func (this *SignalCatcher) RegisterHandler(fn func(), sigs ...int) error {
+
+	for _, index := range sigs {
+		if index > len(signals) {
+			return fmt.Errorf("signal cannot register : %d", index)
+		}
+
+		sig := signals[index]
+		this.handlerMap[sig] = fn
+	}
+	return nil
 }
 
 func (this *SignalCatcher) Close() {
+	signal.Stop(this.sigChan)
 	close(this.quitChan)
 }
 
 func (this *SignalCatcher) Go() {
-	signal.Notify(this.sigChan,)
-}
+	signal.Notify(this.sigChan, signals...)
 
-func (this *SignalCatcher) checkSignal() {
 	for {
 		select {
-		case <- this.quitChan:
+		case sig, more := <-this.sigChan:
+			if more {
+				if fn, ok := this.handlerMap[sig]; ok {
+					fn()
+				}
+			}
+
+		case <-this.quitChan:
 			return
-		case sig := <-this.sigChan:
-			if fn, ok := s.tr
 
 		}
 	}
+
 }
--- go/src/test.go
+++ go/src/test.go
@@ -6,7 +6,8 @@ import (
 	"flog"
 	"time"
 	"sync"
-	"signal"
+    "signal"
+    "fmt"
 )
 
 //func threeSum(nums []int) [][]int {
@@ -53,21 +54,37 @@ func writeFile(logger *flog.Log, index int) {
 }
 
 
+func testSignals() {
+    watcher := signal.NewSignalCatcher()
+    watcher.RegisterHandler(func(){fmt.Println("SIGTERM")}, signal.SIGTERM)
+    watcher.RegisterHandler(func(){fmt.Println("SIGQUIT")}, signal.SIGQUIT)
+    watcher.RegisterHandler(func(){fmt.Println("SIGKILL")}, signal.SIGKILL)
+    watcher.RegisterHandler(func(){fmt.Println("SIGINT")}, signal.SIGINT)
+    watcher.RegisterHandler(func(){fmt.Println("SIGUSR1")}, signal.SIGUSR1)
+    watcher.RegisterHandler(func(){fmt.Println("SIGUSR2")}, signal.SIGUSR2)
+    go watcher.Go()
+}
+
+func testFilelog() {
+    logger := flog.NewLogger("./logs/", "", "test", flog.LOGLEVEL_INFO, 10, 0, flog.LOGSHIFT_TYPE_MINUTE)
+    wg := sync.WaitGroup{}
+    for i:=0; i< 50; i++ {
+        wg.Add(1)
+        go writeFile(logger, i)
+    }
+
+    wg.Wait()
+    logger.Close()
+}
+
+
 func main() {
 	//t1 := time.Now()
 	//fmt.Printf("%v", t1.Add(time.Second*20).Sub(t1))
 
-	signal.NewSignalCatcher()
+    testSignals()
 
-	logger := flog.NewLogger("./logs/", "", "test", flog.LOGLEVEL_INFO, 10, 0, flog.LOGSHIFT_TYPE_MINUTE)
-	wg := sync.WaitGroup{}
-	for i:=0; i< 50; i++ {
-		wg.Add(1)
-		go writeFile(logger, i)
-	}
 
-	wg.Wait()
-	logger.Close()
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 100)
 
 }
